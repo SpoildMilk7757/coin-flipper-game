9ea2aef74fe05408c37410e3427f15677193429f
SpoildMilk7757 <101339945+SpoildMilk7757@users.noreply.github.com>
2022-03-30 18:23:07 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,21 @@ input.onGesture(Gesture.Shake, function () {
     coin1 = randint(0, 1)
     coin2 = randint(0, 1)
     if (coin1 == 0 && coin2 == 0) {
-        basic.showString("2 heads, P1 wins")
+        basic.showString("2 H, P1 wins")
     }
     if (coin1 == 1 && coin2 == 1) {
-        basic.showString("2 tails, P1 wins")
+        basic.showString("2 T, P1 wins")
     }
     if (coin1 == 1 && coin2 == 0 || coin1 == 0 && coin2 == 1) {
-        basic.showString("1 heads 1 tails, P2 wins")
+        basic.showString("1 H 1 T, P2 wins")
     }
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
 })
 let coin2 = 0
 let coin1 = 0
